7620d3ba8530e96d7e1bcb308270875dd542252c
Spanish <maximbasov1992@mail.ru>
2021-08-27 14:49:12 +0300

    refactor

--- utils/cors-options.js
+++ utils/cors-options.js
@@ -1,7 +1,7 @@
 const corsOptions = {
   origin: [
     'http://movies.Spanish.nomoredomains.monster',
-    'https://api.movies.Spanish.nomoredomains.monster',
+    'https://movies.Spanish.nomoredomains.monster',
     'http://api.movies.Spanish.nomoredomains.monster',
     'https://api.movies.Spanish.nomoredomains.monster',
     'http://localhost:3000',
