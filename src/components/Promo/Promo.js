7599965ef76eb271b02eab797208731f8e8df7f1
Spanish <maximbasov1992@mail.ru>
2021-09-06 21:05:59 +0300

    feat: create an adaptive profile

---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Promo.css'
+// import illustration from "../../images/landing-logo.svg";
+
+export default function Promo() {
+  return (
+    <section className="promo">
+      <div className="promo__container">
+        <div className="promo__text-container">
+          <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
+        </div>
+        
+      </div>
+    </section>
+  )
+}
