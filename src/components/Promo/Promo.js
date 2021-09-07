2d3c768f1ab7ff5d6a72bf9d9fcc5b5e45e9b8b0
Spanish <maximbasov1992@mail.ru>
2021-09-07 16:16:13 +0300

    feat: create an adaptive promo-links

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -9,8 +9,12 @@ export default function Promo() {
         <div className="promo__text-container">
           <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
         </div>
-        
       </div>
+      <div className="promo__navigation">
+          <a className="promo__navigation-link" href='#about-project'>О проекте</a>
+          <a className="promo__navigation-link" href='#techs'>Технологии</a>
+          <a className="promo__navigation-link" href='#profile'>Студент</a>
+        </div>
     </section>
   )
 }
